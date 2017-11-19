57a255168b6fececeb7edbf53230bd2d2d9df0ee
chenxuuu <lolicon@papapoi.com>
2017-11-19 09:51:13 +0800

    防崩溃

--- play.py
+++ play.py
@@ -15,47 +15,49 @@ def convert_time(n):
     return '00:'+"%02d"%m+':'+"%02d"%s
 
 while True:
-    files = os.listdir(path+'/downloads')
-    count=0
-    for f in files:
-        if(f.find('.mp3') != -1):
-            audio = MP3(path+'/downloads/'+f)
+    try:
+        files = os.listdir(path+'/downloads')
+        count=0
+        for f in files:
+            if(f.find('.mp3') != -1):
+                audio = MP3(path+'/downloads/'+f)
+                seconds=audio.info.length   #获取时长
+                print('mp3 long:'+convert_time(seconds))
+                if(seconds > 600):
+                    print('too long,delete')
+                else:
+                    pic_files = os.listdir(path+'/default_pic')
+                    pic_ran = random.randint(0,len(pic_files)-1)
+                    print('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/downloads/'+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp3",'')+'.ass'+'" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+                    os.system('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/downloads/'+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp3",'')+'.ass'+'" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+                try:
+                    os.remove(path+'/downloads/'+f)
+                    os.remove(path+'/downloads/'+f.replace(".mp3",'')+'.ass')
+                except:
+                    print('delete error')
+                count+=1
+            if(f.find('.mp4') != -1):
+                print('mp4:'+f)
+                print('ffmpeg -re -i "'+path+"/downloads/"+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp4",'')+'.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+                os.system('ffmpeg -re -i "'+path+"/downloads/"+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp4",'')+'.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+                try:
+                    os.remove(path+'/downloads/'+f)
+                    os.remove(path+'/downloads/'+f.replace(".mp4",'')+'.ass')
+                except:
+                    print('delete error')
+                count+=1
+        if(count == 0):
+            print('no media')
+            mp3_files = os.listdir(path+'/default_mp3')
+            pic_files = os.listdir(path+'/default_pic')
+            mp3_ran = random.randint(0,len(mp3_files)-1)
+            pic_ran = random.randint(0,len(pic_files)-1)
+            audio = MP3(path+'/default_mp3/'+mp3_files[mp3_ran])
             seconds=audio.info.length   #获取时长
             print('mp3 long:'+convert_time(seconds))
-            if(seconds > 600):
-                print('too long,delete')
-            else:
-                pic_files = os.listdir(path+'/default_pic')
-                pic_ran = random.randint(0,len(pic_files)-1)
-                print('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/downloads/'+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp3",'')+'.ass'+'" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-                os.system('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/downloads/'+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp3",'')+'.ass'+'" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-            try:
-                os.remove(path+'/downloads/'+f)
-                os.remove(path+'/downloads/'+f.replace(".mp3",'')+'.ass')
-            except:
-                print('delete error')
-            count+=1
-        if(f.find('.mp4') != -1):
-            print('mp4:'+f)
-            print('ffmpeg -re -i "'+path+"/downloads/"+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp4",'')+'.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-            os.system('ffmpeg -re -i "'+path+"/downloads/"+f+'" -vf ass="'+path+"/downloads/"+f.replace(".mp4",'')+'.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-            try:
-                os.remove(path+'/downloads/'+f)
-                os.remove(path+'/downloads/'+f.replace(".mp4",'')+'.ass')
-            except:
-                print('delete error')
-            count+=1
-    if(count == 0):
-        print('no media')
-        mp3_files = os.listdir(path+'/default_mp3')
-        pic_files = os.listdir(path+'/default_pic')
-        mp3_ran = random.randint(0,len(mp3_files)-1)
-        pic_ran = random.randint(0,len(pic_files)-1)
-        audio = MP3(path+'/default_mp3/'+mp3_files[mp3_ran])
-        seconds=audio.info.length   #获取时长
-        print('mp3 long:'+convert_time(seconds))
-        print('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-        os.system('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
-
+            print('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+            os.system('ffmpeg -re -s 1280x720 -loop 1 -r 2 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+    except:
+        print('shit')
 
         
