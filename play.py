cb4f68e8f3bded1b8cc51ea72b03ec1f869b0e4d
chenxuuu <lolicon@papapoi.com>
2017-11-20 18:57:46 +0800

    自定义歌曲支持mp4

--- play.py
+++ play.py
@@ -54,15 +54,21 @@ while True:
             print('no media')
             mp3_files = os.listdir(path+'/default_mp3')
             mp3_files.sort()
-            pic_files = os.listdir(path+'/default_pic')
-            pic_files.sort()
             mp3_ran = random.randint(0,len(mp3_files)-1)
-            pic_ran = random.randint(0,len(pic_files)-1)
-            audio = MP3(path+'/default_mp3/'+mp3_files[mp3_ran])
-            seconds=audio.info.length   #获取时长
-            print('mp3 long:'+convert_time(seconds))
-            print('ffmpeg -re -s 1280x720 -loop 1 -r 3 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -c:v h264_omx -f flv "'+rtmp+live_code+'"')
-            os.system('ffmpeg -re -s 1280x720 -loop 1 -r 3 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -c:v h264_omx -f flv "'+rtmp+live_code+'"')
+            
+            if(mp3_files[mp3_ran].find('.mp3') != -1):
+                pic_files = os.listdir(path+'/default_pic')
+                pic_files.sort()
+                pic_ran = random.randint(0,len(pic_files)-1)
+                audio = MP3(path+'/default_mp3/'+mp3_files[mp3_ran])
+                seconds=audio.info.length   #获取时长
+                print('mp3 long:'+convert_time(seconds))
+                print('ffmpeg -re -s 1280x720 -loop 1 -r 3 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -c:v h264_omx -f flv "'+rtmp+live_code+'"')
+                os.system('ffmpeg -re -s 1280x720 -loop 1 -r 3 -t '+str(int(seconds))+' -f image2 -i "'+path+'/default_pic/'+pic_files[pic_ran]+'" -i "'+path+'/default_mp3/'+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -pix_fmt yuv420p -crf 24 -preset ultrafast -maxrate 1000k -acodec aac -b:a 192k -c:v h264_omx -f flv "'+rtmp+live_code+'"')
+            if(mp3_files[mp3_ran].find('.mp4') != -1):
+                print('ffmpeg -re -i "'+path+"/default_mp3/"+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+                os.system('ffmpeg -re -i "'+path+"/default_mp3/"+mp3_files[mp3_ran]+'" -vf ass="'+path+'/default.ass" -vcodec libx264 -preset ultrafast -acodec aac -b:a 192k -f flv "'+rtmp+live_code+'"')
+
     except:
         print('shit')
 
