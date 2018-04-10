b601d1ee648937384b8ac16f0e7e3591573f31fd
chenxuuu <lolicon@papapoi.com>
2018-04-10 18:54:17 +0800

    优化两处判断，防炸

--- bilibiliClient.py
+++ bilibiliClient.py
@@ -203,19 +203,24 @@ class bilibiliClient():
             return
         return
 
-danmuji = bilibiliClient()
+while True: #防炸
+    try:
+        danmuji = bilibiliClient()
 
-tasks = [
-            danmuji.connectServer() ,
-            danmuji.HeartbeatLoop()
-        ]
-loop = asyncio.get_event_loop()
-try:
-    loop.run_until_complete(asyncio.wait(tasks))
-except KeyboardInterrupt:
-    danmuji.connected = False
-    for task in asyncio.Task.all_tasks():
-        task.cancel()
-    loop.run_forever()
+        tasks = [
+                    danmuji.connectServer() ,
+                    danmuji.HeartbeatLoop()
+                ]
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(asyncio.wait(tasks))
+        except KeyboardInterrupt:
+            danmuji.connected = False
+            for task in asyncio.Task.all_tasks():
+                task.cancel()
+            loop.run_forever()
 
-loop.close()
+        loop.close()
+    except Exception as e:  #防炸
+        print('shit')
+        print(e)
