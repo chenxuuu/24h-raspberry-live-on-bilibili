72109fff8ccb54064dc05ecfbc29aa40dd0c491d
chenxuuu <lolicon@papapoi.com>
2018-12-23 16:11:17 +0800

    更正发送弹幕时使用的参数，close #49

--- post_dm.py
+++ post_dm.py
@@ -575,19 +575,20 @@ def send_dm(s):
         'fontsize':'25',
         'mode':'1',
         'msg':s,
-        'rnd':'1510756027',
+        'rnd':str(int(time.time())),
         'roomid':roomid,
-        'csrf_token':csrf_token
+        'csrf_token':csrf_token,
+        'csrf':csrf_token
         }).encode('utf-8')
         header = {
-        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept":"application/json, text/javascript, */*; q=0.01",
         "Accept-Encoding":"utf-8",
         "Accept-Language":"zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
         "Connection":"keep-alive",
         "Cookie":cookie,
         "Host":"api.live.bilibili.com",
         "Referer":"http://live.bilibili.com/"+roomid,
-        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 Vivaldi/2.2.1388.37"
         }
         req = urllib.request.Request(url,postdata,header)
         dm_result = json.loads(urllib.request.urlopen(req,timeout=3).read().decode('utf-8'))
@@ -596,8 +597,9 @@ def send_dm(s):
             print(dm_result)
         else:
             print('[log]发送弹幕：'+s)
-    except:
+    except Exception as e:
         print('[error]send dm error')
+        print(e)
     time.sleep(1.5)
     dm_lock = False
 
